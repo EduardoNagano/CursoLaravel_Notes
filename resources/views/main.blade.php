a2d2236e94119c23675fed5c4000b406d7ffa8f6
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-11-29 11:38:58 -0300

    005

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,7 +10,11 @@
     
     <h1>Welcome View and Blade</h1>
     <hr>
-    <h3>The value is: <?= $value ?></h3>
+    <h3>The value is: <?= $value ?> em PHP exemplo 1</h3>
+    
+    <h3>The value is: <?php echo $value ?> em PHP exemplo 2</h3>
+
+    <h3>The value is: {{ $value }} em Blade</h3>
 
 </body>
 </html>
