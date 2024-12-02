e3a8e8ab2e3a6b8844ef72b9e728b4bd7efdd989
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-02 10:34:12 -0300

    006

--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -3,9 +3,6 @@
 @section('content')
     <h1>Welcome View and Blade</h1>
     <hr>
-    <h3>The value is: <?= $value ?> em PHP exemplo 1</h3>
-
-    <h3>The value is: <?php echo $value ?> em PHP exemplo 2</h3>
-
+    <h3>Page 3</h3>
     <h3>The value is: {{ $value }} em Blade</h3>
 @endsection
