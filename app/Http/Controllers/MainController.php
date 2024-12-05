ae2f38d39a015c125f69b12bfec9872ce3156b9e
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-05 15:38:57 -0300

    029

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -14,12 +16,33 @@ class MainController extends Controller
         $notes = User::find($id)->notes()->get()->toArray();
 
         // show home view
-        return view('home', ['notes' =>$notes]);
-        
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
     {
         echo "I'm creating a new note!";
+    }
+
+    public function editNote($id)
+    {
+        try {
+            $id = Crypt::decrypt($id);
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        echo "I'm editing note with id = " . $id;
+    }
+
+    public function deleteNote($id)
+    {
+        try {
+            $id = Crypt::decrypt($id);
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        echo "I'm deleting note with id = " . $id;
     }
 }
