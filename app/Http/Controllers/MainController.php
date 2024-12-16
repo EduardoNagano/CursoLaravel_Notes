360823ac0d12a9ff1914c6fb2024ef4877e81687
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-16 11:34:30 -0300

    037

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,11 @@ class MainController extends Controller
     {
         // load user's notes
         $id = session('user.id');
-        $notes = User::find($id)->notes()->get()->toArray();
+        $notes = User::find($id)
+            ->notes()
+            ->whereNull('deleted_at')
+            ->get()
+            ->toArray();
 
         // show home view
         return view('home', ['notes' => $notes]);
@@ -114,7 +118,32 @@ class MainController extends Controller
 
     public function deleteNote($id)
     {
+        // check if $id is encrypted
         $id = Operations::decryptId($id);
-        echo "I'm deleting note with id = " . $id;
+
+        // load note
+        $note = Note::find($id);
+
+        // show delete note confirmation
+        return view('delete_note', ['note' => $note]);
+    }
+
+    public function deleteNoteConfirm($id)
+    {
+        // check if $id is encrypted
+        $id = Operations::decryptId($id);
+
+        // load note
+        $note = Note::find($id);
+
+        // 1. hard delete
+        // $note->delete();
+
+        // 2. soft delete
+        $note->deleted_at = date('Y-m-d H:i:s');
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 }
